133ef264d5bcfafdeb3d8dbbcd3149cebbefe0ce
jack <wulingjian123@gmail.com>
2018-08-05 15:21:43 +0800

    mv old reamde file to the folder

--- testcode/test_time.go
+++ testcode/test_time.go
@@ -5,12 +5,29 @@ import (
 	"time"
 )
 
+func LastMonth(t time.Time) time.Time {
+	deltaDay := 0
+	for {
+		lastMonth := t.AddDate(0, -1, deltaDay)
+		if lastMonth.Month() == t.Month() {
+			deltaDay--
+			continue
+		}
+		return lastMonth
+	}
+	return time.Time{}
+}
 func main() {
 	//fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
-	p:=fmt.Println
+	p := fmt.Println
 
 	// 当前时间
-	p(time.Now().Format("2006-01-02 15:04:05"))
+	// p(time.Now().Format("2006-01-02 15:04:05"))
 
-	p(time.Now().Sub(time.Now()).Hours())
+	t := time.Now().UnixNano()
+	p(t)
+	// t := time.Now()
+	// newT := t.AddDate(0, -1, 0).Format("2006-01-02")
+	// p(newT)
+	// p(LastMonth(time.Now().AddDate(0, -1, 26)).Format("2006-01-02"))
 }
