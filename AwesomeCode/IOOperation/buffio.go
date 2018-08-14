0d0f9fe7d0b6ad407320e543c3e6c0290129bf13
jack <wulingjian123@gmail.com>
2018-08-14 09:24:06 +0800

    commit before merge

--- AwesomeCode/IOOperation/buffio.go
+++ AwesomeCode/IOOperation/buffio.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"bufio"
+	"os"
+	"fmt"
+)
+
+func buffRead()  {
+	reader:=bufio.NewReader(os.Stdin)
+
+	reader.ReadLine()
+	str,err:=reader.ReadString('\n')
+	if err==nil{
+		fmt.Println("input was:",str)
+	}
+
+}
+
 func main() {
-	$END$
+	//fmt.Println(string('A'),string('B'))
+	buffRead()
 }
